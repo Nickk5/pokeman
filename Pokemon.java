fc295350256fa0868fa838c6e5210abd8a8401fc
Nickk5 <558443@my.cuhsd.org>
2024-12-15 15:59:43 -0800

    worked on creating pokemon, the game loop, and add variables to Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,19 +1,49 @@
 public class Pokemon
 {
+  private String name;
+  private String[] type;
   private int atk;
   private int def;
   private int spatk;
   private int spdef;
   private int hp;
   private int spd;
+  private double atkchange;
+  private double defchange;
+  private double spdchange;
+  private double spdefchange;
+  private double spatkchange;
 
-  public Pokemon(int atk, int def, int spatk, int spdef, int hp, int spd) {
+  public Pokemon(String name, String[] type, int atk, int def, int spatk, int spdef, int hp, int spd) {
+    this.name = name;
+    this.type = type;
     this.atk = atk;
     this.def = def;
     this.spatk = spatk;
     this.spdef = spdef;
     this.hp = hp;
     this.spd = spd;
+    atkchange = 1;
+    defchange = 1;
+    spdchange = 1;
+    spdefchange = 1;
+    spatkchange = 1;
+  }
+
+  public String[] getType() {
+    return type;
+  }
+
+  public void setType(String[] type) {
+    this.type = type;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
   public int getAtk() {
@@ -62,5 +92,45 @@ public class Pokemon
 
   public void setSpd(int spd) {
     this.spd = spd;
+  }
+
+  public double getSpatkchange() {
+    return spatkchange;
+  }
+
+  public void setSpatkchange(double spatkchange) {
+    this.spatkchange = spatkchange;
+  }
+
+  public double getSpdefchange() {
+    return spdefchange;
+  }
+
+  public void setSpdefchange(double spdefchange) {
+    this.spdefchange = spdefchange;
+  }
+
+  public double getSpdchange() {
+    return spdchange;
+  }
+
+  public void setSpdchange(double spdchange) {
+    this.spdchange = spdchange;
+  }
+
+  public double getDefchange() {
+    return defchange;
+  }
+
+  public void setDefchange(double defchange) {
+    this.defchange = defchange;
+  }
+
+  public double getAtkchange() {
+    return atkchange;
+  }
+
+  public void setAtkchange(double atkchange) {
+    this.atkchange = atkchange;
   }
 }
--- main.java
+++ main.java
@@ -1,10 +1,57 @@
+import java.util.Scanner;
+
 public class main
   {
   public static void main(String[] args)
     {
+      Pokemon[] player = new Pokemon[6];
+      int numPokemon = 0;
       TypeChart test = new TypeChart();
-      String attack = "Fighting";
-      String[] type = {"Steel", "Rock"};
-      System.out.println(test.getMultiplier(attack, type));
+      Scanner in = new Scanner(System.in);
+      for(int i = 0; i < player.length; i++)
+      {
+        System.out.println("Enter Pokemon Name: ");
+        String name = in.nextLine();
+        System.out.println("Enter pokemon atk: ");
+        int atk = in.nextInt();
+        System.out.println("Enter pokemon def: ");
+        int def = in.nextInt();
+        System.out.println("Enter pokemon speed: ");
+        int speed = in.nextInt();
+        System.out.println("Enter pokemon health: ");
+        int health = in.nextInt();
+        System.out.println("Enter pokemon spatk: ");
+        int spatk = in.nextInt();
+        System.out.println("Enter pokemon spdef: ");
+        int spdef = in.nextInt();
+        in.nextLine();
+        System.out.println("Enter the pokemon's first type: ");
+        String type1 = in.nextLine();
+        System.out.println("Enter the pokemon's second type(Enter none if there is no second type): ");
+        String type2 = in.nextLine();
+        String[] type = {type1, type2};
+        player[i] = new Pokemon(name, type, atk, def, spatk, spdef, health, speed);
+        numPokemon++;
+        System.out.println("Do you want to add more pokemon to your team?(y/n)");
+        if(in.next().equalsIgnoreCase("n"))
+        {
+          in.nextLine();
+          break;
+        }
+        in.nextLine();
+      }
+      // for(int i = 0; i < numPokemon; i++)
+      // {
+        // System.out.println(test.getMultiplier("Fighting", player[i].getType()));
+        //System.out.println(player[i].getName());
+      // }
+      int numRounds = 0;
+      while(true)
+      {
+        numRounds++;
+
+        break;
+      }
+      System.out.println("You survived " + numRounds + " rounds!");
     }
   }
