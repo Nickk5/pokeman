a71bbe7826e0d6cd861c2a216dd15cc5bc0b8418
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2024-12-15 21:35:42 -0800

    Create Pokemon with make.java

---
+++ Pokemon with make.java
@@ -0,0 +1,31 @@
+public static Pokemon createPokemon(Scanner in) {
+    System.out.println("Enter Pokémon Name: ");
+    String name = in.nextLine();
+
+    System.out.println("Enter Pokémon Attack: ");
+    int atk = validateStatInput(in, "Attack");
+
+    System.out.println("Enter Pokémon Defense: ");
+    int def = validateStatInput(in, "Defense");
+
+    System.out.println("Enter Pokémon Speed: ");
+    int speed = validateStatInput(in, "Speed");
+
+    System.out.println("Enter Pokémon Health: ");
+    int health = validateStatInput(in, "Health");
+
+    System.out.println("Enter Pokémon Special Attack: ");
+    int spatk = validateStatInput(in, "Special Attack");
+
+    System.out.println("Enter Pokémon Special Defense: ");
+    int spdef = validateStatInput(in, "Special Defense");
+
+    System.out.println("Enter Pokémon's first type: ");
+    String type1 = in.nextLine();
+
+    System.out.println("Enter Pokémon's second type (Enter 'none' if there is no second type): ");
+    String type2 = in.nextLine();
+    String[] type = type2.equalsIgnoreCase("none") ? new String[]{type1} : new String[]{type1, type2};
+
+    return new Pokemon(name, type, atk, def, spatk, spdef, health, speed);
+}
