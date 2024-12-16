e2c806e1f99561513f33758a8ad22eafb76c08e9
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2024-12-15 21:45:43 -0800

    Create SpecialMove.java

---
+++ SpecialMove.java
@@ -0,0 +1,39 @@
+public class SpecialMove implements Moves {
+    private String type;
+    private int basePower;
+    private double accuracy;
+    private int priority;
+
+    public SpecialMove(String type, int basePower, double accuracy, int priority) {
+        this.type = type;
+        this.basePower = basePower;
+        this.accuracy = accuracy;
+        this.priority = priority;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public int getBasePower() {
+        return basePower;
+    }
+
+    @Override
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public double getDamage(Pokemon attacker, Pokemon defender) {
+        double typeMultiplier = new TypeChart().getMultiplier(type, defender.getType());
+        return basePower * ((double) attacker.getSpatk() / defender.getSpdef()) * typeMultiplier;
+    }
+}
