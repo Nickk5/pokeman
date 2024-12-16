d34f29bf931afd5736bd4231431a1fd3dadc08c3
Nicholas Kicinski <558443@my.cuhsd.org>
2024-12-16 11:32:38 -0800

    fixing code

--- Moves.java
+++ Moves.java
@@ -1,8 +1,41 @@
+/**
+ * an interface to represent the move that a pokemon may know
+ */
 public interface Moves {
+    /**
+     *
+     * @param attacker attacking pokemon
+     * @param defender defending pokemon
+     * @return amount of damage
+     */
     double getDamage(Pokemon attacker, Pokemon defender);
+    /**
+     *
+     * @return typing of the move
+     */
     String getType();
+
+    /**
+     *
+     * @return base power of the move
+     */
     int getBasePower();
+
+    /**
+     *
+     * @return number of hits that the move deals
+     */
     int getNumHits();
+
+    /**
+     *
+     * @return priority of the move
+     */
     int getPriority();
+
+    /**
+     *
+     * @return the accuracy of the move
+     */
     double getAccuracy();
 }
