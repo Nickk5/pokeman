1e474718dc27b158f658606dccdecf5ea4ce4f1a
Nicholas Kicinski <558443@my.cuhsd.org>
2024-12-16 11:39:06 -0800

    javadocs

--- SpecialMove.java
+++ SpecialMove.java
@@ -1,9 +1,20 @@
+/**
+ * a class to represent a special move
+ */
 public class SpecialMove implements Moves {
     private String type;
     private int basePower;
     private double accuracy;
     private int priority;
     private int numHits;
+
+    /**
+     *
+     * @param type the typing of the move
+     * @param basePower the base power of the move
+     * @param accuracy the accuracy of the move
+     * @param priority the priority of the move
+     */
 
     public SpecialMove(String type, int basePower, double accuracy, int priority) {
         this.type = type;
@@ -12,30 +23,57 @@ public class SpecialMove implements Moves {
         this.priority = priority;
     }
 
+    /**
+     *
+     * @return the type of the move
+     */
     @Override
     public String getType() {
         return type;
     }
 
+    /**
+     *
+     * @return base power of the move
+     */
     @Override
     public int getBasePower() {
         return basePower;
     }
 
+    /**
+     *
+     * @return accuracy of the move
+     */
     @Override
     public double getAccuracy() {
         return accuracy;
     }
 
+    /**
+     *
+     * @return priority of the move
+     */
     @Override
     public int getPriority() {
         return priority;
     }
 
+    /**
+     *
+     * @return the number of hits that the move would deal
+     */
     public int getNumHits()
     {
         return numHits;
     }
+
+    /**
+     *
+     * @param attacker attacking pokemon
+     * @param defender defending pokemon
+     * @return the damage the move would deal with the pokemon damage formula
+     */
     @Override
     public double getDamage(Pokemon attacker, Pokemon defender) {
         double typeMultiplier = new TypeChart().getMultiplier(type, defender.getType());
