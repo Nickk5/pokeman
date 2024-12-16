cce5dee22f282ad90e15db0db98d0e7e90f59b30
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2024-12-15 21:40:16 -0800

    Update Pokemon with make.java

--- Pokemon with make.java
+++ Pokemon with make.java
@@ -27,24 +27,18 @@ public static Pokemon createPokemon(Scanner in) {
     String type2 = in.nextLine();
     String[] type = type2.equalsIgnoreCase("none") ? new String[]{type1} : new String[]{type1, type2};
 
-    return new Pokemon(name, type, atk, def, spatk, spdef, health, speed);
-}
-
-private static int validateStatInput(Scanner in, String statName) {
-    int stat;
-    while (true) {
-        try {
-            System.out.printf("Enter %s (0-255): ", statName);
-            stat = in.nextInt();
-            if (stat >= 0 && stat <= 255) {
-                break;
-            } else {
-                System.out.println("Invalid input. Please enter a value between 0 and 255.");
-            }
-        } catch (Exception e) {
-            System.out.println("Invalid input. Please enter a number.");
-            in.next(); // Clear invalid input
+    Moves[] moves = new Moves[4];
+    for (int i = 0; i < 4; i++) {
+        System.out.printf("Enter type for Move %d: ", i + 1);
+        String moveType = in.nextLine();
+        System.out.printf("Is Move %d Physical or Special? (Enter 'Physical' or 'Special'): ", i + 1);
+        String moveCategory = in.nextLine();
+        if (moveCategory.equalsIgnoreCase("Special")) {
+            moves[i] = new SpecialMove(moveType, 80, 0.95, 0, "Extra Effect " + (i + 1));
+        } else {
+            moves[i] = new PhysicalMove(moveType, 80, 0.95, 0);
         }
     }
-    return stat;
+
+    return new Pokemon(name, type, atk, def, spatk, spdef, health, speed, moves);
 }
