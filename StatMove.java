f858555d83b1bc6bc90fadc5a2597c6ef22fd933
Nicholas Kicinski <558443@my.cuhsd.org>
2024-12-16 11:44:10 -0800

    javadocs

--- StatMove.java
+++ StatMove.java
@@ -1,14 +1,27 @@
+/**
+ * a move to represent a move that deals no damage but changes the stat of a pokemon
+ */
 public class StatMove implements Moves {
     private String affectedStat;
     private double changeAmount;
     private double accuracy;
     private int basePower;
 
+    /**
+     *
+     * @return base power of the move
+     */
     @Override
     public int getBasePower() {
         return basePower;
     }
 
+    /**
+     *
+     * @param affectedStat the stat that is being affected
+     * @param changeAmount the amount the affected stat is being changed by
+     * @param accuracy the accuracy of the move
+     */
     public StatMove(String affectedStat, double changeAmount, double accuracy) {
         this.affectedStat = affectedStat;
         this.changeAmount = changeAmount;
@@ -16,6 +29,10 @@ public class StatMove implements Moves {
         basePower = 0;
     }
 
+    /**
+     * changes the affected stat of the target pokemon by the change amount
+     * @param target the pokemon's target
+     */
     public void applyEffect(Pokemon target) {
         switch (affectedStat.toLowerCase()) {
             case "attack": target.setAtkchange(target.getAtkchange() * changeAmount); break;
@@ -23,26 +40,48 @@ public class StatMove implements Moves {
         }
     }
 
+    /**
+     *
+     * @param attacker attacking pokemon
+     * @param defender defending pokemon
+     * @return zero because the move will deal no damage
+     */
     @Override
     public double getDamage(Pokemon attacker, Pokemon defender) {
         return 0;
     }
 
+    /**
+     *
+     * @return no type because status moves have no type
+     */
     @Override
     public String getType() {
         return "None";
     }
 
+    /**
+     *
+     * @return zero because the move does not hit the opponent
+     */
     @Override
     public int getNumHits() {
         return 0;
     }
 
+    /**
+     *
+     * @return priority of the pokemon
+     */
     @Override
     public int getPriority() {
         return 0;
     }
 
+    /**
+     *
+     * @return accuracy of the pokemon
+     */
     @Override
     public double getAccuracy() {
         return accuracy;
