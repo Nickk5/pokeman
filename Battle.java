c790dae8f731f253268fa41056a3a0f3b8a836cf
Yejun Shin <91703836+YejunShin@users.noreply.github.com>
2024-12-16 11:35:42 -0800

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -1,75 +1,61 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Battle {
-    private Pokemon playerPokemon;
-    private NPCPokemon npcPokemon;
 
-    public Battle(Pokemon playerPokemon, NPCPokemon npcPokemon) {
-        this.playerPokemon = playerPokemon;
-        this.npcPokemon = npcPokemon;
-    }
+    public void startBattle(Pokemon player, NPCPokemon npc) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("A battle has started between " + player.getName() + " and " + npc.getName());
 
-    public void startBattle() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("A battle has started between " + playerPokemon.getName() + " and " + npcPokemon.getName() + "!");
+        boolean battleOver = false;
+        while (!battleOver) {
+            // Player's turn
+            System.out.println(player.getName() + "'s turn!");
 
-        // Determine the turn order using speed algorithm
-        Pokemon[] battleParticipants = {playerPokemon, npcPokemon};
-        speedAlgorithm speedAlg = new speedAlgorithm();
-        Queue<Pokemon> turnOrder = speedAlg.speedCheck(battleParticipants);
-
-        while (playerPokemon.getHp() > 0 && npcPokemon.getHp() > 0) {
-            Pokemon currentPokemon = turnOrder.poll(); // Get the next Pokémon to act
-
-            // Perform turn based on who is first
-            if (currentPokemon.equals(playerPokemon)) {
-                playerTurn(scanner);
-                if (npcPokemon.getHp() <= 0) {
-                    System.out.println(npcPokemon.getName() + " has been defeated!");
-                    System.out.println("You win!");
-                    return;
-                }
-            } else {
-                npcTurn();
-                if (playerPokemon.getHp() <= 0) {
-                    System.out.println(playerPokemon.getName() + " has been defeated!");
-                    System.out.println("You lose!");
-                    return;
-                }
+            // Show the player's available moves
+            for (int i = 0; i < player.getMoves().length; i++) {
+                System.out.println((i + 1) + ". " + player.getMoves()[i].getType() + " (Power: " 
+                    + player.getMoves()[i].getBasePower() + ")");
             }
-            // Recalculate the turn order after each round based on the remaining Pokémon
-            turnOrder = speedAlg.speedCheck(new Pokemon[] {playerPokemon, npcPokemon});
+
+            // Prompt player to choose a move
+            System.out.println("Choose a move by entering the number (1-" + player.getMoves().length + "): ");
+            int choice = in.nextInt();
+            in.nextLine();  // Consume the newline character
+
+            // Ensure the choice is valid
+            if (choice >= 1 && choice <= player.getMoves().length) {
+                Moves playerMove = player.getMoves()[choice - 1];
+                System.out.println(player.getName() + " used " + playerMove.getType() + "!");
+                npc.inflictDamage(playerMove.getDamage(player, npc));
+                System.out.println(npc.getName() + " took damage, HP is now " + npc.getHp());
+            } else {
+                System.out.println("Invalid choice. Please select a valid move.");
+                continue;  // Skip the current loop iteration if the move is invalid
+            }
+
+            // Check if NPC Pokemon is fainted
+            if (npc.getHp() <= 0) {
+                System.out.println(npc.getName() + " has fainted! " + player.getName() + " wins!");
+                battleOver = true;
+                break;
+            }
+
+            // NPC's turn
+            npcTurn(player, npc);
+
+            // Check if Player Pokemon is fainted
+            if (player.getHp() <= 0) {
+                System.out.println(player.getName() + " has fainted! " + npc.getName() + " wins!");
+                battleOver = true;
+            }
         }
     }
 
-    private void playerTurn(Scanner scanner) {
-        System.out.println("\nIt's your turn! Choose a move:");
-
-        // List available moves for player
-        for (int i = 0; i < playerPokemon.getMoves().length; i++) {
-            Moves move = playerPokemon.getMoves()[i];
-            System.out.println((i + 1) + ". " + move.getType() + " (Power: " + move.getBasePower() + ", Accuracy: " + move.getAccuracy() + ")");
-        }
-
-        // Get player's move choice
-        int moveChoice = scanner.nextInt();
-        if (moveChoice >= 1 && moveChoice <= playerPokemon.getMoves().length) {
-            Moves selectedMove = playerPokemon.getMoves()[moveChoice - 1];
-            double damage = selectedMove.getDamage(playerPokemon, npcPokemon);
-            npcPokemon.inflictDamage(damage);
-            System.out.println(playerPokemon.getName() + " used " + selectedMove.getType() + "!");
-            System.out.println(npcPokemon.getName() + " took " + damage + " damage!");
-        } else {
-            System.out.println("Invalid move. Please choose again.");
-            playerTurn(scanner);
-        }
-    }
-
-    private void npcTurn() {
-        Moves npcMove = npcPokemon.chooseMove(playerPokemon);
-        double damage = npcMove.getDamage(npcPokemon, playerPokemon);
-        playerPokemon.inflictDamage(damage);
-        System.out.println(npcPokemon.getName() + " used " + npcMove.getType() + "!");
-        System.out.println(playerPokemon.getName() + " took " + damage + " damage!");
+    private void npcTurn(Pokemon player, NPCPokemon npc) {
+        System.out.println(npc.getName() + "'s turn!");
+        Moves npcMove = npc.chooseMove(player); // NPC chooses a move
+        System.out.println(npc.getName() + " used " + npcMove.getType() + "!");
+        player.inflictDamage(npcMove.getDamage(npc, player));
+        System.out.println(player.getName() + " took damage, HP is now " + player.getHp());
     }
 }
